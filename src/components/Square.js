1eeda8b1bd144f5b9de702171a3441773b75b847
John Kiernan <jkiernan12@yahoo.com>
2022-01-10 18:12:05 -0500

    Fix Square text size and aspect ratio

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ const Square = ({ id, className, space, name, stamp, flagPath }) => {
 
     return (
         <div className={className} onClick={() => stamp(id, space)}>
-            <p>{name}</p>
+            <p className="country-name">{name}</p>
             <img className="flag-image" src={flagPath} />
         </div>
     )
