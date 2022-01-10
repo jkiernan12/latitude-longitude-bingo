c6759fcd41b7b391575dca930ce5cdc520956ee5
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2022-01-10 17:18:27 -0500

    Add stub for fetch call

--- cypress/integration/stubbed_spec.js
+++ cypress/integration/stubbed_spec.js
@@ -2,11 +2,334 @@ describe('async stubbing', () => {
   beforeEach(() => {
     cy.intercept('get', 'http://localhost:8080/api/v1/countries', {
       statusCode: 200,
-      body: [{"id":"b915129d-96e4-4de1-bc2f-a8d9b46876a8","regions":{"data":["Europe","Southern Europe"]},"code":"AD","name":"Andorra","latitude":"42.55","longitude":"1.60","flag_path":"./assets/flags/ad.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"4819e088-f3ba-4159-9e10-0ba4d389f4b0","regions":{"data":["Asia","Western Asia"]},"code":"AE","name":"United Arab Emirates","latitude":"23.42","longitude":"53.85","flag_path":"./assets/flags/ae.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"c378e2cf-0e26-4db5-a44b-e4c78baaa228","regions":{"data":["Asia","Southern Asia"]},"code":"AF","name":"Afghanistan","latitude":"33.94","longitude":"67.71","flag_path":"./assets/flags/af.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"281c5907-984f-438a-ae8a-82e2e045914a","regions":{"data":["Americas","Latin America and the Caribbean","Caribbean"]},"code":"AG","name":"Antigua and Barbuda","latitude":"17.06","longitude":"-61.80","flag_path":"./assets/flags/ag.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"785894c1-a199-4461-bb5c-dad9d8642603","regions":{"data":["Americas","Latin America and the Caribbean","Caribbean"]},"code":"AI","name":"Anguilla","latitude":"18.22","longitude":"-63.07","flag_path":"./assets/flags/ai.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"8772195d-11c0-49fa-8f06-5799ab16ecb9","regions":{"data":["Europe","Southern Europe"]},"code":"AL","name":"Albania","latitude":"41.15","longitude":"20.17","flag_path":"./assets/flags/al.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"59abd1b9-28bc-4764-a6dd-3d7e8e3f90a0","regions":{"data":["Asia","Western Asia"]},"code":"AM","name":"Armenia","latitude":"40.07","longitude":"45.04","flag_path":"./assets/flags/am.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"b0ba2dc4-6d3c-4a1b-985e-245e773517f2","regions":{"data":[]},"code":"AN","name":"Netherlands Antilles","latitude":"12.23","longitude":"-69.06","flag_path":"./assets/flags/an.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"f830aa92-ec9c-4b14-a84c-c06a06174dbb","regions":{"data":["Africa","Sub-Saharan Africa","Middle Africa"]},"code":"AO","name":"Angola","latitude":"-11.20","longitude":"17.87","flag_path":"./assets/flags/ao.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"},{"id":"ed110ab0-a046-4a94-a5d5-7f9e691fc883","regions":{"data":[""]},"code":"AQ","name":"Antarctica","latitude":"-75.25","longitude":"-0.07","flag_path":"./assets/flags/aq.svg","created_at":"2022-01-05T21:38:07.628Z","updated_at":"2022-01-05T21:38:07.628Z"}]
-    })
+      body: [{
+        "id": "f830aa92-ec9c-4b14-a84c-c06a06174dbb",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Middle Africa"
+            ]
+        },
+        "code": "AO",
+        "name": "Angola",
+        "latitude": "-11.20",
+        "longitude": "17.87",
+        "flag_path": "./assets/flags/ao.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    }, {
+        "id": "daaf3c16-a1f3-457d-922a-97f9e8fa0f17",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Western Africa"
+            ]
+        },
+        "code": "BF",
+        "name": "Burkina Faso",
+        "latitude": "12.24",
+        "longitude": "-1.56",
+        "flag_path": "./assets/flags/bf.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "5f01f330-e547-401c-a230-d326c7de0db5",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Eastern Africa"
+            ]
+        },
+        "code": "BI",
+        "name": "Burundi",
+        "latitude": "-3.37",
+        "longitude": "29.92",
+        "flag_path": "./assets/flags/bi.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "086ad08d-3bd8-4c46-a0a5-ec9b686c4a7f",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Western Africa"
+            ]
+        },
+        "code": "BJ",
+        "name": "Benin",
+        "latitude": "9.31",
+        "longitude": "2.32",
+        "flag_path": "./assets/flags/bj.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "1ffd28d9-646f-4fc4-9ba2-08d27962b4bb",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Southern Africa"
+            ]
+        },
+        "code": "BW",
+        "name": "Botswana",
+        "latitude": "-22.33",
+        "longitude": "24.68",
+        "flag_path": "./assets/flags/bw.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "bfa072ca-5698-47c1-b04e-863854c18c86",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Middle Africa"
+            ]
+        },
+        "code": "CD",
+        "name": "Congo [DRC]",
+        "latitude": "-4.04",
+        "longitude": "21.76",
+        "flag_path": "./assets/flags/cd.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "f51483ff-2870-4ce3-8868-e8d5b18b59a2",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Middle Africa"
+            ]
+        },
+        "code": "CF",
+        "name": "Central African Republic",
+        "latitude": "6.61",
+        "longitude": "20.94",
+        "flag_path": "./assets/flags/cf.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },
+    {
+        "id": "34f5fe16-3966-4ab6-95ec-719d0d4a01ea",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Middle Africa"
+            ]
+        },
+        "code": "CG",
+        "name": "Congo [Republic]",
+        "latitude": "-0.23",
+        "longitude": "15.83",
+        "flag_path": "./assets/flags/cg.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "bf5c2fc6-d39a-4463-9178-3cc79daa163c",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Western Africa"
+            ]
+        },
+        "code": "CI",
+        "name": "Côte d'Ivoire",
+        "latitude": "7.54",
+        "longitude": "-5.55",
+        "flag_path": "./assets/flags/ci.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    }, {
+        "id": "f46ca99a-a6f2-4368-8396-e879e42db76e",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Middle Africa"
+            ]
+        },
+        "code": "CM",
+        "name": "Cameroon",
+        "latitude": "7.37",
+        "longitude": "12.35",
+        "flag_path": "./assets/flags/cm.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "b0919c26-dc05-4d2d-901e-c11a5fefda19",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Western Africa"
+            ]
+        },
+        "code": "CV",
+        "name": "Cape Verde",
+        "latitude": "16.00",
+        "longitude": "-24.01",
+        "flag_path": "./assets/flags/cv.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "16c0facd-d306-4185-8f7d-5848c47149c9",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Eastern Africa"
+            ]
+        },
+        "code": "DJ",
+        "name": "Djibouti",
+        "latitude": "11.83",
+        "longitude": "42.59",
+        "flag_path": "./assets/flags/dj.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "1c980abf-76e2-4009-87a3-5b9953665d6d",
+        "regions": {
+            "data": [
+                "Africa",
+                "Northern Africa"
+            ]
+        },
+        "code": "DZ",
+        "name": "Algeria",
+        "latitude": "28.03",
+        "longitude": "1.66",
+        "flag_path": "./assets/flags/dz.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    }, {
+        "id": "089cb1c0-a3b2-4980-9228-219ca3fc5bd2",
+        "regions": {
+            "data": [
+                "Africa",
+                "Northern Africa"
+            ]
+        },
+        "code": "EG",
+        "name": "Egypt",
+        "latitude": "26.82",
+        "longitude": "30.80",
+        "flag_path": "./assets/flags/eg.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    }, {
+        "id": "af56d3b6-1bb1-497a-9852-9a7b1915134e",
+        "regions": {
+            "data": [
+                "Africa",
+                "Northern Africa"
+            ]
+        },
+        "code": "EH",
+        "name": "Western Sahara",
+        "latitude": "24.22",
+        "longitude": "-12.89",
+        "flag_path": "./assets/flags/eh.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },
+    {
+        "id": "9879ac59-121f-4de9-9789-c823e4196309",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Eastern Africa"
+            ]
+        },
+        "code": "ER",
+        "name": "Eritrea",
+        "latitude": "15.18",
+        "longitude": "39.78",
+        "flag_path": "./assets/flags/er.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "ab498e40-d62c-4132-9941-b4070f466bef",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Eastern Africa"
+            ]
+        },
+        "code": "ET",
+        "name": "Ethiopia",
+        "latitude": "9.15",
+        "longitude": "40.49",
+        "flag_path": "./assets/flags/et.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "e5770a53-35e5-47bf-ae43-694b4ba65c69",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Middle Africa"
+            ]
+        },
+        "code": "GA",
+        "name": "Gabon",
+        "latitude": "-0.80",
+        "longitude": "11.61",
+        "flag_path": "./assets/flags/ga.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    },{
+        "id": "f3891af0-777b-40f5-ace2-dec4a9a71db1",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Western Africa"
+            ]
+        },
+        "code": "GH",
+        "name": "Ghana",
+        "latitude": "7.95",
+        "longitude": "-1.02",
+        "flag_path": "./assets/flags/gh.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    }, {
+        "id": "e63b54f0-ecc5-4238-90b0-538c2de7a999",
+        "regions": {
+            "data": [
+                "Africa",
+                "Sub-Saharan Africa",
+                "Western Africa"
+            ]
+        },
+        "code": "GM",
+        "name": "Gambia",
+        "latitude": "13.44",
+        "longitude": "-15.31",
+        "flag_path": "./assets/flags/gm.svg",
+        "created_at": "2022-01-05T21:38:07.628Z",
+        "updated_at": "2022-01-05T21:38:07.628Z"
+    }]
+  }).as('africa-abridged')
     cy.visit('http://localhost:3000/')
   })
-  it('should be to serve coordinates', () => {
-    cy.get('.Home--link').contains('South America').click()
+  it('should be able to serve coordinates', () => {
+    cy.get('.Home--link').contains('Africa').click();
+    cy.get('.bingo-btn').click();
+    cy.get('.next-btn').click();
+    cy.get('h3').contains(/S|N/)
+    cy.get('h3').next().contains(/E|W/)
   })
 })
